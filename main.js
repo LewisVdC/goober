d8db5ab3271a1c93d68ef2b48c54d5cc1c78549c
Lewisvdc <lewis.vandencruyce@leerling.kov.be>
2025-04-30 09:34:32 +0200

    nu uh

--- main.js
+++ main.js
@@ -4556,7 +4556,12 @@ function checkAchievement() {
       6000
     );
   }
-  if (achievement.buyAllRedUpgrades === false && redupgrade1===1 && redupgrade2 ===1 && redupgrade3===1) {
+  if (
+    achievement.buyAllRedUpgrades === false &&
+    redupgrade1 === 1 &&
+    redupgrade2 === 1 &&
+    redupgrade3 === 1
+  ) {
     achievement.buyAllRedUpgrades = true;
     achievementItemImg[3].src = achievementNotificationImg.src =
       "images/achievements/buyAllRedUpgrades.webp";
@@ -4609,7 +4614,12 @@ function checkAchievement() {
       6000
     );
   }
-  if (achievement.buyAllGreenUpgrades === false && greenupgrade1===1 && greenupgrade2 ===1 && greenupgrade3===1) {
+  if (
+    achievement.buyAllGreenUpgrades === false &&
+    greenupgrade1 === 1 &&
+    greenupgrade2 === 1 &&
+    greenupgrade3 === 1
+  ) {
     achievement.buyAllGreenUpgrades = true;
     achievementItemImg[9].src = achievementNotificationImg.src =
       "images/achievements/buyAllGreenUpgrades.webp";
@@ -4662,7 +4672,12 @@ function checkAchievement() {
       6000
     );
   }
-  if (achievement.buyAllBlueUpgrades === false && blueupgrade1===1 && blueupgrade2 ===1 && blueupgrade3===1) {
+  if (
+    achievement.buyAllBlueUpgrades === false &&
+    blueupgrade1 === 1 &&
+    blueupgrade2 === 1 &&
+    blueupgrade3 === 1
+  ) {
     achievement.buyAllBlueUpgrades = true;
     achievementItemImg[15].src = achievementNotificationImg.src =
       "images/achievements/buyAllBlueUpgrades.webp";
@@ -4674,5 +4689,20 @@ function checkAchievement() {
       "achievementNotification.classList.remove('slide');",
       6000
     );
+  }
+}
+
+//
+//
+//
+//
+//
+
+// spells
+// and
+// stuff
+
+function spell(spelltype) {
+  if (spelltype == 1) {
   }
 }
